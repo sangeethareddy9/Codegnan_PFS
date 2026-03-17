db65b7bc3f4191385b577f1e5d5165e625f1d250
abhiram9306 <abhiram44399@gmail.com>
2026-03-17 13:22:09 +0530

    My first Push and personal profile

---
+++ Day 1 .py
@@ -0,0 +1,1 @@
+print("Iam Sangeetha, I joined in codegnan")
---
+++ My details.py
@@ -0,0 +1,7 @@
+print("My Name is *Sangeetha")
+print("Iam from Andhra University")
+print("2025 Passedout with 8.53 CGPA")
+print("       -*-*-*-*-*-*-*    ")
+print("I joined in Codegnan recently to crack the job")
+print("The course is 'Python Full Stack Development' of 100 days")
+
